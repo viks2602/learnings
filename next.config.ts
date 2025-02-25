2232cac53b21c1b64b0a24ef2de26cfdac3e33f0
Vikas B Patil <viks.0226@gmail.com>
2025-02-25 11:08:01 +0530

    prisma user-based models

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // typescript:{
+  //   //Will still allow production builds if there are type errors
+  //   ignoreBuildErrors: true,
+  // },
+  images:{
+    remotePatterns:[
+      {
+        protocol: 'https',
+        hostname:'utfs.io',
+        port:''
+      }
+    ]
+  }
 };
 
 export default nextConfig;
